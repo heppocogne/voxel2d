cd0e52b93268244a88767d249fdd6ad344994e50
heppocogne <83043568+heppocogne@users.noreply.github.com>
2023-01-03 21:42:00 +0900

    Update player

---
+++ gameplay/entity/Entity.cs
@@ -0,0 +1,37 @@
+using Godot;
+using System;
+
+public class Entity : KinematicBody2D
+{
+    [Export]
+    public float MaxFallSpeed = 384;
+
+    // state
+    public int ChunkPosition = 0;
+    public Vector2 Velocity = Vector2.Zero;
+
+    // physics
+    protected int gravity;
+    protected float airDiag;
+    protected Vector2 gravityVector;
+
+    // graphics
+    protected bool facingRight = true;   // true=right, false=left
+    protected AnimatedSprite sprite;
+
+    public override void _Ready()
+    {
+        gravity = (int)ProjectSettings.GetSetting("physics/2d/default_gravity");
+        gravityVector = (Vector2)ProjectSettings.GetSetting("physics/2d/default_gravity_vector");
+        airDiag = gravity / (MaxFallSpeed * MaxFallSpeed);
+        sprite = GetNode<AnimatedSprite>("AnimatedSprite");
+    }
+
+    public override void _PhysicsProcess(float delta)
+    {
+        Velocity += (gravity - airDiag * Velocity.y * Velocity.y) * delta * gravityVector;
+        Velocity = MoveAndSlide(Velocity, Vector2.Up);
+
+        ChunkPosition = (int)(Position.x / (Chunk.ChunkSize * Chunk.CellSize.x));
+    }
+}
---
+++ gameplay/entity/player/Player.cs
@@ -0,0 +1,65 @@
+using Godot;
+using System;
+
+public class Player : Entity
+{
+    [Export]
+    public float WalkSpeed = 96;
+    [Export]
+    public float RunSpeed = 128;
+    [Export]
+    public float JumpHeight = 20;
+
+
+
+    // physics
+    float jumpInitialVelocity;
+
+    public override void _Ready()
+    {
+        base._Ready();
+        jumpInitialVelocity = (float)Math.Sqrt(gravity * JumpHeight);
+    }
+
+    public override void _PhysicsProcess(float delta)
+    {
+        base._PhysicsProcess(delta);
+
+        bool l = Input.IsActionPressed("game_left");
+        bool r = Input.IsActionPressed("game_right");
+        if (l && !r)
+        {
+            MoveAndCollide(new Vector2(-WalkSpeed, 0) * delta);
+            sprite.Play("left_walk");
+            facingRight = false;
+        }
+        else if (r && !l)
+        {
+            MoveAndCollide(new Vector2(WalkSpeed, 0) * delta);
+            sprite.Play("right_walk");
+            facingRight = true;
+        }
+        else
+        {
+            if (facingRight)
+            {
+                sprite.Play("right_walk");
+                sprite.Playing = false;
+                sprite.Frame = 1;
+            }
+            else
+            {
+                sprite.Play("left_walk");
+                sprite.Playing = false;
+                sprite.Frame = 1;
+            }
+        }
+
+        if (IsOnFloor() && Input.IsActionJustPressed("game_jump"))
+        {
+            Velocity = new Vector2(Velocity.x, -jumpInitialVelocity);
+        }
+
+        Velocity = MoveAndSlide(Velocity, Vector2.Up);
+    }
+}
