c99d92e18c459b2c2033723529c5fccb67fa0d5b
heppocogne <83043568+heppocogne@users.noreply.github.com>
2023-01-03 22:14:34 +0900

    Set viewport scale

--- gameplay/game_screen.gd
+++ gameplay/game_screen.gd
@@ -1,11 +1,15 @@
 extends Control
 
+export var viewport_scale:=2.0
 onready var viewport:Viewport=$ViewportContainer/Viewport
 
 
 func _ready():
 	get_tree().root.connect("size_changed", self, "_on_Window_size_changed")
+	_on_Window_size_changed()
 
 
 func _on_Window_size_changed():
+	viewport.set_size_override(true,OS.window_size/viewport_scale)
+	viewport.set_size_override_stretch(true)
 	viewport.size=OS.window_size
