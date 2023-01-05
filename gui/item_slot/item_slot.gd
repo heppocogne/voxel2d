b4c5397c6b4d83d85f49c6024a8a0ee897054a6d
heppocogne <83043568+heppocogne@users.noreply.github.com>
2023-01-05 13:01:06 +0900

    Update item_slot

--- gui/item_slot/item_slot.gd
+++ gui/item_slot/item_slot.gd
@@ -2,6 +2,10 @@ tool
 extends TextureRect
 
 export var focused:=false setget set_focused
+var item_name:String
+
+onready var item_texture:TextureRect=$CenterContainer/TextureRect
+onready var label:Label=$Label
 
 
 func _ready():
@@ -11,3 +15,18 @@ func _ready():
 func set_focused(f:bool):
 	focused=f
 	$BoldFrame.visible=focused
+
+
+func _on_item_changed(tex:Texture,name:String):
+	item_name=name
+	item_texture.texture=tex
+
+
+func _on_number_changed(n:int):
+	if n==0:
+		_on_item_changed(null,"")
+		label.visible=false
+	elif n==1:
+		label.visible=false
+	else:
+		label.text=str(n)
