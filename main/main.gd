f5e659a30e26fda6d80bd9d5dbbd7f29303fac85
heppocogne <83043568+heppocogne@users.noreply.github.com>
2023-01-08 20:34:23 +0900

    Update main screen

--- main/main.gd
+++ main/main.gd
@@ -1,6 +1,25 @@
 extends Control
 
+var delta_sum:=0.0
+onready var logo:TextureRect=$CenterContainer/VBoxContainer/TextureRect
+
 
 func _ready():
 	randomize()
+	_on_Main_resized()
+
+
+func _on_Main_resized():
+	$TextureRect.rect_position=(rect_size-$TextureRect.rect_size)/2
+
+
+func _on_NewGame_pressed():
 	get_tree().change_scene_to(preload("res://gameplay/game_screen.tscn"))
+
+
+func _on_LoadGame_pressed():
+	pass # Replace with function body.
+
+
+func _on_Exit_pressed():
+	get_tree().quit(0)
