b889a10a98a3dc5981acdf3bd2baaea5385d301f
heppocogne <83043568+heppocogne@users.noreply.github.com>
2023-01-10 09:02:15 +0900

    Put an item with right click in inventory

--- gui/inventory/inventory_slot.gd
+++ gui/inventory/inventory_slot.gd
@@ -49,38 +49,57 @@ func _input(event:InputEvent):
 	if event is InputEventMouseButton:
 		var mb:=event as InputEventMouseButton
 		if mb.pressed:
-			if mb.button_index==BUTTON_LEFT and Rect2(Vector2.ZERO,rect_size).has_point(get_local_mouse_position()):
-				if display_name=="":
-					if gui_root.grabbed_item:
-						# place
-						set_item_info(gui_root.grabbed_item.display_name,gui_root.grabbed_item.texture,gui_root.grabbed_item.quantity)
-						emit_signal("item_released")
-						emit_signal("state_changed")
-				else:
-					var grabbed:GrabbedItem=preload("res://gui/inventory/grabbed_item.tscn").instance()
-					grabbed.set_item_info(display_name,item_texture.texture,quantity)
-					if gui_root.grabbed_item:
-						if gui_root.grabbed_item.display_name==grabbed.display_name:
-							# merge
-							var max_stack:int=world_node.FindItemData(grabbed.display_name)["Stack"]
-							if gui_root.grabbed_item.quantity+quantity<=max_stack:
-								set_item_quantity(quantity+gui_root.grabbed_item.quantity)
-								emit_signal("item_released")
-							else:
-								var diff:=max_stack-quantity
-								set_item_quantity(quantity+diff)
-								gui_root.grabbed_item.set_item_quantity(gui_root.grabbed_item.quantity-diff)
-						else:
-							# swap
+			if Rect2(Vector2.ZERO,rect_size).has_point(get_local_mouse_position()):
+				if mb.button_index==BUTTON_LEFT:
+					if display_name=="":
+						if gui_root.grabbed_item:
+							# place
 							set_item_info(gui_root.grabbed_item.display_name,gui_root.grabbed_item.texture,gui_root.grabbed_item.quantity)
 							emit_signal("item_released")
-							emit_signal("item_grabbed",grabbed)
-						emit_signal("state_changed")
+							emit_signal("state_changed")
 					else:
-						# grab
-						emit_signal("item_grabbed",grabbed)
-						emit_signal("state_changed")
-						clear()
+						var grabbed:GrabbedItem=preload("res://gui/inventory/grabbed_item.tscn").instance()
+						grabbed.set_item_info(display_name,item_texture.texture,quantity)
+						if gui_root.grabbed_item:
+							if gui_root.grabbed_item.display_name==display_name:
+								# merge
+								var max_stack:int=world_node.FindItemData(display_name)["Stack"]
+								if gui_root.grabbed_item.quantity+quantity<=max_stack:
+									set_item_quantity(quantity+gui_root.grabbed_item.quantity)
+									emit_signal("item_released")
+								else:
+									var diff:=max_stack-quantity
+									set_item_quantity(quantity+diff)
+									gui_root.grabbed_item.set_item_quantity(gui_root.grabbed_item.quantity-diff)
+							else:
+								# swap
+								set_item_info(gui_root.grabbed_item.display_name,gui_root.grabbed_item.texture,gui_root.grabbed_item.quantity)
+								emit_signal("item_released")
+								emit_signal("item_grabbed",grabbed)
+							emit_signal("state_changed")
+						else:
+							# grab
+							emit_signal("item_grabbed",grabbed)
+							emit_signal("state_changed")
+							clear()
+							
+				elif mb.button_index==BUTTON_RIGHT:
+					if gui_root.grabbed_item:
+						if display_name=="":
+							set_item_info(gui_root.grabbed_item.display_name,gui_root.grabbed_item.texture,0)
+						if gui_root.grabbed_item.display_name==display_name:
+							# add an item
+							var max_stack:int=world_node.FindItemData(display_name)["Stack"]
+							if gui_root.grabbed_item.quantity<max_stack:
+								set_item_quantity(quantity+1)
+								gui_root.grabbed_item.set_item_quantity(gui_root.grabbed_item.quantity-1)
+								if gui_root.grabbed_item.quantity==0:
+									emit_signal("item_released")
+								emit_signal("state_changed")
+					else:
+						# split
+						var half:=quantity/2
+						
 
 
 func set_item_quantity(n:int):
